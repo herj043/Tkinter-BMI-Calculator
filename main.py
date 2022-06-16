8f2d97fbda8993faac5821757bcc8fb2dccc7d61
Jeremy Her <91761028+herj043@users.noreply.github.com>
2022-06-16 12:29:39 +0000

    added text boxes

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 import tkinter as tk
 
 class smallText:
-  def __init__(self, root, txt = "NONE", r = 0, c = 0):
+  def __init__(self, root, txt = "NONE", r = 0, c = 0, column_span = 1):
     self.window = tk.Label(root, text=txt)
-    self.column_span = 1
-    self.row = r
-    self.column = c
+    self.window.grid(row=r, column=c, columnspan= column_span)
 
-  def create_win(self):
-    self.window.grid(row=self.row, column=self.column, columnspan = self.column_span)
+class insertText:
+  def __init__(self, root, wide = 1, r = 0, c = 0, column_span = 1):
+    self.window = tk.Entry(root, width= wide)
+    self.window.grid(row=r, column=c, columnspan= column_span)
+
+  def __repr__(self):
+    return int(self.window.get())
+
+def find_bmi(ft, inch, weight):
+  pass
 
 
-# Variable
 bmi_win = tk.Tk()
 bmi_win.title("BMI Calculator")
 
 text = smallText(bmi_win, """Welcome To BMI Calculator.
-Enter your weight and height below.""", 0, 0)
-text.column_span = 3
-text.create_win()
+Enter your weight and height below.""", 0, 0, 5)
+
 
 frame = smallText(bmi_win, "- - - - - - - - - - - - - - - - -"\
-+ " - - - - - - - ", 1, 0)
-frame.column_span = 5
-frame.create_win()
++ " - - - - - - - ", 1, 0, 5)
 
 feet = smallText(bmi_win, "ft", 2, 0)
-feet.create_win()
+feet_text_box = insertText(bmi_win, 5, 2, 1)
 
 inch = smallText(bmi_win, "in", 3, 0)
-inch.create_win()
+inch_text_box = insertText(bmi_win, 5, 3, 1)
 
-results = tk.Button(bmi_win, text="Get Results")
-results.grid(row=4,column=0, columnspan=3)
+weight = smallText(bmi_win, "weight(lbs)", 2, 4)
+weight_text_box = insertText(bmi_win, 10, 3, 4)
+
+
+results = tk.Button(bmi_win, text="Get Results", command=lambda: find_bmi(feet_text_box, inch_text_box, weight_text_box))
+results.grid(row=4,column=0, columnspan=5)
 
 
 
